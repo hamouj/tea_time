37b540c4de2ff258c4441d19900cc67b13d0dee3
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-05-30 14:28:43 -0700

    fix: update factories for new tea/subscription relationship

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :subscription do
-    association :tea
-
     title { Faker::Restaurant.name }
     price { Faker::Number.between(from: 1000, to: 6000) }
     status { "active" }
