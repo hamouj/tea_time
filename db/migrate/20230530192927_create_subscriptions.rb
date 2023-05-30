464351dcc486e566c9b3d7cef13ec2936b327c2d
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-05-30 12:34:11 -0700

    chore: create database

---
+++ db/migrate/20230530192927_create_subscriptions.rb
@@ -0,0 +1,13 @@
+class CreateSubscriptions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :subscriptions do |t|
+      t.string :title
+      t.integer :price
+      t.integer :status, default: 0
+      t.integer :frequency
+      t.references :tea, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
