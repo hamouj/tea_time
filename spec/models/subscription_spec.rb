84e6d6c784304796405c27bc61945442275d0170
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-05-30 14:26:17 -0700

    test: create tests for new assocations

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe Subscription, type: :model do
   describe "relationships" do
     it { should have_many :customer_subscriptions }
-    it { should belong_to :tea }
+    it { should have_many :tea_subscriptions }
   end
 
   describe "enums" do
