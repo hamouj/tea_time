00a2a772dae44ff2b57d0a32df525eaac138ab29
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-05-30 14:37:36 -0700

    chore: add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
 customer1 = Customer.create!({ first_name: "Jasmine", last_name: "Hamou", email: "j@gmail.com", address: "123 Neverland Ave, Las Vegas, NV 12345"})
 customer2 = Customer.create!({ first_name: "Hailey", last_name: "Harper", email: "h@gmail.com", address: "456 Imaginary St, Las Vegas, NV 45678"})
 customer3 = Customer.create!({ first_name: "Ivan", last_name: "Andrew", email: "i@gmail.com", address: "789 North Ave, Las Vegas, NV 78910"})
 
 tea1 = Tea.create!({ title: "Earl Grey", description: "tastes like morning", temperature: 208, brew_time: 4 })
-tea1 = Tea.create!({ title: "Peppermint", description: "tastes minty", temperature: 200, brew_time: 3 })
-tea1 = Tea.create!({ title: "Green Tea", description: "tastes like the energizer bunny", temperature: 180, brew_time: 4 })
+tea2 = Tea.create!({ title: "Peppermint", description: "tastes minty", temperature: 200, brew_time: 3 })
+tea3 = Tea.create!({ title: "Green Tea", description: "tastes like the energizer bunny", temperature: 180, brew_time: 4 })
 
-subscription1 = Subscription.create!({ title: "Get Your Tea On", price:  })
-subscription2 = create(:subscription, tea_id:1 )
-subscription3 = create(:subscription, tea_id:2 )
-subscription4 = create(:subscription, tea_id:3 )
-subscription5 = create(:cancelled_subscription, tea_id:3 )
+subscription1 = Subscription.create!({ title: "Get Your Tea On", price: 5500, status: 0, frequency: 1 })
+subscription2 = Subscription.create!({ title: "Tea Yeah", price: 6230, status: 0, frequency: 2 })
+subscription3 = Subscription.create!({ title: "Pepperminty", price: 2310, status: 0, frequency: 3 })
+subscription4 = Subscription.create!({ title: "I'm Feeling Green", price: 1250, status: 0, frequency: 2 })
+subscription5 = Subscription.create!({ title: "Tea Much?", price: 4550, status: 1, frequency: 1 })
 
-create(:customer_subscription, customer: customer1, subscription: subscription1)
-create(:customer_subscription, customer: customer1, subscription: subscription5)
-create(:customer_subscription, customer: customer2, subscription: subscription2)
-create(:customer_subscription, customer: customer2, subscription: subscription3)
-create(:customer_subscription, customer: customer3, subscription: subscription4)
+CustomerSubscription.create!({ customer: customer1, subscription: subscription1 })
+CustomerSubscription.create!({ customer: customer1, subscription: subscription5 })
+CustomerSubscription.create!({ customer: customer2, subscription: subscription2 })
+CustomerSubscription.create!({ customer: customer2, subscription: subscription3 })
+CustomerSubscription.create!({ customer: customer3, subscription: subscription4 })
+
+TeaSubscription.create!({ tea: tea1, subscription: subscription1 })
+TeaSubscription.create!({ tea: tea3, subscription: subscription1 })
+
+TeaSubscription.create!({ tea: tea1, subscription: subscription2 })
+TeaSubscription.create!({ tea: tea2, subscription: subscription2 })
+TeaSubscription.create!({ tea: tea3, subscription: subscription2 })
+
+TeaSubscription.create!({ tea: tea2, subscription: subscription3 })
+
+TeaSubscription.create!({ tea: tea3, subscription: subscription4 })
+
+TeaSubscription.create!({ tea: tea2, subscription: subscription5 })
+TeaSubscription.create!({ tea: tea3, subscription: subscription5 })
