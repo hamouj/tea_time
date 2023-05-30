b788f2b83f77accfd4b5f47b2a5a68b70f45caad
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-05-30 13:39:21 -0700

    fix: update tea temperature from float to integer

--- spec/factories/teas.rb
+++ spec/factories/teas.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :tea do
     title { Faker::Tea.variety }
     description { Faker::Cofee.notes }
-    temperature { Faker::Number.between(from: 170.00, to: 212.00) }
+    temperature { Faker::Number.between(from: 170, to: 212) }
     brew_time { Faker::Number.between(from: 60, to: 240) }
   end
 end
