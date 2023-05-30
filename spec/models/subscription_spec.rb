b12d93d265f7924be1899f0e9fec8eddcee72f92
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-05-30 13:57:20 -0700

    test: add test for status enum

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Subscription, type: :model do
     it { should have_many :customer_subscriptions }
     it { should belong_to :tea }
   end
+
+  describe "enums" do
+    it { should define_enum_for(:status).with_values(["active", "cancelled"])}
+  end
 end
