e373b210c4acc88d8ad06bd72ab112a7fe544623
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-05-31 14:05:02 -0700

    feat: add create action for customer subscription resource

---
+++ app/controllers/api/v1/customer_subscriptions_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::CustomerSubscriptionsController < ApplicationController
+
+  def create
+    customer_subscription = CustomerSubscription.new(customer_subscription_params)
+    customer_subscription.save!
+    render json: CustomerSubscriptionSerializer.new(customer_subscription), status: 201
+  end
+
+  private
+  def customer_subscription_params
+    params.require(:customer_subscription).permit(:customer_id, :subscription_id)
+  end
+end
