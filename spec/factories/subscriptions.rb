43aae46ed139088cbb84f0e55d133ead663aea3f
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-06-01 12:55:10 -0700

    fix: updat factory for chang of status from active live

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :subscription do
     title { Faker::Restaurant.name }
     price { Faker::Number.between(from: 1000, to: 6000) }
-    status { "active" }
+    status { "live" }
     frequency { Faker::Number.between(from: 1, to: 5) }
 
     trait :inactive do
