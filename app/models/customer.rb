cfdb04bc8a7d52e0302f353b10d77bd3e4efcb55
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-06-01 12:53:59 -0700

    feat: create live subscriptions model method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,8 @@
 class Customer < ApplicationRecord
   has_many :customer_subscriptions, dependent: :destroy
+  has_many :subscriptions, through: :customer_subscriptions
+
+  def live_subscriptions
+    subscriptions.merge(Subscription.live)
+  end
 end
