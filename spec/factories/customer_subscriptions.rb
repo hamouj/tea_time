ac8832967632aefe48f2907d8bbc17d45c8a788c
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-06-01 08:54:43 -0700

    test: add status to customer subscription factory

--- spec/factories/customer_subscriptions.rb
+++ spec/factories/customer_subscriptions.rb
@@ -2,5 +2,12 @@ FactoryBot.define do
   factory :customer_subscription do
     association :customer
     association :subscription
+    status { "active" }
+
+    trait :cancelled do
+      status { "cancelled" }
+    end
+
+    factory :cancelled_subscription, traits: [:cancelled]
   end
 end
