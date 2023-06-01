c6e92a2bed71d48e60b6acf5a4fa3678cc089411
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-06-01 08:44:54 -0700

    fix: update seed data to include status for customer subscription

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+Customer.destroy_all
+Subscription.destroy_all
+Tea.destroy_all
+
 customer1 = Customer.create!({ first_name: "Jasmine", last_name: "Hamou", email: "j@gmail.com", address: "123 Neverland Ave, Las Vegas, NV 12345"})
 customer2 = Customer.create!({ first_name: "Hailey", last_name: "Harper", email: "h@gmail.com", address: "456 Imaginary St, Las Vegas, NV 45678"})
 customer3 = Customer.create!({ first_name: "Ivan", last_name: "Andrew", email: "i@gmail.com", address: "789 North Ave, Las Vegas, NV 78910"})
@@ -12,11 +16,11 @@ subscription3 = Subscription.create!({ title: "Pepperminty", price: 2310, status
 subscription4 = Subscription.create!({ title: "I'm Feeling Green", price: 1250, status: 0, frequency: 2 })
 subscription5 = Subscription.create!({ title: "Tea Much?", price: 4550, status: 1, frequency: 1 })
 
-CustomerSubscription.create!({ customer: customer1, subscription: subscription1 })
-CustomerSubscription.create!({ customer: customer1, subscription: subscription5 })
-CustomerSubscription.create!({ customer: customer2, subscription: subscription2 })
-CustomerSubscription.create!({ customer: customer2, subscription: subscription3 })
-CustomerSubscription.create!({ customer: customer3, subscription: subscription4 })
+CustomerSubscription.create!({ customer: customer1, subscription: subscription1, status: 0 })
+CustomerSubscription.create!({ customer: customer1, subscription: subscription5, status: 0 })
+CustomerSubscription.create!({ customer: customer2, subscription: subscription2, status: 1 })
+CustomerSubscription.create!({ customer: customer2, subscription: subscription3, status: 0 })
+CustomerSubscription.create!({ customer: customer3, subscription: subscription4, status: 0 })
 
 TeaSubscription.create!({ tea: tea1, subscription: subscription1 })
 TeaSubscription.create!({ tea: tea3, subscription: subscription1 })
