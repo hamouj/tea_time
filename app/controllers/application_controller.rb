9da439713e9d4679fab20e331d360abb25cb17db
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-06-01 09:22:51 -0700

    feat: add error handling for record not found error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid, with: :validation_error_response
   rescue_from ActionController::ParameterMissing, with: :validation_error_response
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_error_response
 
   def validation_error_response(error)
     render json: ErrorSerializer.new(error).validation_error, status: :bad_request
+  end
+
+  def record_not_found_error_response(error)
+    render json: ErrorSerializer.new(error).record_not_found_error, status: :not_found
   end
 end
