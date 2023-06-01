a016e44e79e0fcd6ac0672a78e3b618a6763a834
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-06-01 09:28:29 -0700

    fix: pluralize resource in route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resource :customer_subscription, only: [:create, :update]
+      resource :customer_subscriptions, only: [:create, :update]
     end
   end
 end
