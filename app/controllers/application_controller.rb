e0d3fdc0c647256a2d7c6d5c52a2c5e7d42a57b8
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-06-05 12:51:55 -0700

    fix: remove unneeded rescue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid, with: :validation_error_response
-  rescue_from ActionController::ParameterMissing, with: :validation_error_response
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_error_response
 
   def validation_error_response(error)
